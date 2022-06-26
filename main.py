352736dbf88e40795a6d05a7fde71ac96230ab49
Nogella1 <slegon1@gmail.com>
2022-06-26 13:37:47 +0100

    Tidy up with some functions

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ left = 0
 right = 0
 
 #new stuff
+#print(time.time())
 #winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 #winsound.PlaySound("ding.wav", winsound.SND_ASYNC)
 #print(random.uniform(0.7, 1.3))
@@ -26,7 +27,7 @@ wn.bgcolor("white")  # black white pink all OK
 wn.setup(width=800, height=600)  # size of window  0,0 is at the centre
 wn.tracer(0)  # stops screen updating itself
 
-'''# set up the pen
+# set up the pen
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("black")
@@ -34,7 +35,7 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0,260)
 pen.clear()
-pen.write(f"{left}  :  {right}",align="center", font=("Arial", 24, "normal"))'''
+pen.write(f"{left}  :  {right}",align="center", font=("Arial", 24, "bold"))
 
 # Left Paddle pad_L
 pad_L = turtle.Turtle()  # Turtle is a class of the turtle module
@@ -86,7 +87,65 @@ def downR():
     pad_R.sety(y)
 
 def quit_game():
-    pad_R.sety(400)
+    wn.bye()
+
+def check_border():
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        ball.dy *= 1.05
+        ball.dx *= 1.05
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+    elif ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        ball.dy *= 1.05
+        ball.dx *= 1.05
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+def check_score():
+    global left
+    global right
+    if ball.xcor() > 390 or ball.xcor() < -390:
+        if ball.xcor() > 390:
+            left += 1
+        else:
+            right += 1
+        pen.clear()
+        pen.write(f"{left}  :  {right}", align="center", font=("Arial", 24, "bold"))
+        winsound.PlaySound("ding.wav", winsound.SND_ASYNC)
+        if left > 4 or right > 4:
+            time.sleep(0.5)
+            quit()
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+        if random.randint(0, 1) == 0:
+            ball.dy *= -1  # Reverse the Y movement
+        if ball.dx > 0:  # LEFT or RIGHT
+            ball.dx = 3
+        else:
+            ball.dx = -3
+
+        if ball.dy > 0:  # UP or DOWN
+            ball.dy = 3
+        else:
+            ball.dy = -3
+        pad_L.sety(0)
+        pad_R.sety(0)
+
+        delay = True  # Wait a bit
+
+def check_paddles():
+    adjust = random.uniform(0.8, 1.2)       # random between 0.8 & 1.2 so bounce not predictable
+    if (ball.xcor() > 330 and ball.xcor() < 350) and (ball.ycor() < pad_R.ycor() + 50 and ball.ycor() > pad_R.ycor() -50):
+        ball.setx(330)
+        ball.dx *= -1                       # reverse the x direction
+        ball.dy *= adjust                   # adjusted y movement
+    if (ball.xcor() < -330 and ball.xcor() > -350 ) and (ball.ycor() < pad_L.ycor() + 50 and ball.ycor() > pad_L.ycor() -50):
+        ball.setx(-330)
+        ball.dx *= -1
+        ball.dy *= adjust
 
 # Keyboard binding
 wn.listen()
@@ -94,73 +153,40 @@ wn.onkeypress(upL, "w")
 wn.onkeypress(downL, "s")
 wn.onkeypress(upR, "Up")
 wn.onkeypress(downR, "Down")
-#wn.onkeypress(quit_game, "q")
+wn.onkeypress(quit_game, "q")
 
-delay = True
-frame = 0
-start = time.time() # number of seconds at start of game  ***********
+if random.randint(0,1) == 0:    # randomise which way the game starts
+    ball.dy *= -1
+if random.randint(0,1) == 0:
+    ball.dx *= -1
+
+delay = True        # slight pause before we start
+frame = 0           # seconds so far in this frame
+start = time.time() # number of seconds at start of game  ****
 
 # Main game loop
 while True:         # loop until we tell it to stop
     now = time.time()
     frame = now - start
 
-    if frame > 0.02:
+    if frame > 0.02:    # new frame starts here
         start = now
-        wn.update()    # we control when it updates
+        wn.update()    # update the screen
         if delay:
+            pad_L.sety(0)
+            pad_R.sety(0)
+
             time.sleep(0.5)
             delay = False
             start = time.time()
-        #if pad_R.ycor()>350:
-            #quit()
         ball.setx(ball.xcor() + ball.dx)    # move the ball by dx
         ball.sety(ball.ycor() + ball.dy)    # move the ball by dy
 
-        # Top Border checking
-        if ball.ycor() > 290:
-            ball.sety(290)
-            ball.dy *= -1
-            ball.dy *= 1.1
-            ball.dx *= 1.1
-        elif ball.ycor() < -290:
-            ball.sety(-290)
-            ball.dy *= -1
-            ball.dy *= 1.1
-            ball.dx *= 1.1
+        check_border()      # Border checking
+        check_score()       # Check for score
+        check_paddles()     # check the paddles
 
-        # Check for score
-        if ball.xcor()> 390 or ball.xcor()< -390:
-            if ball.xcor() > 390:
-                left += 1
-            else:
-                right += 1
-            #pen.clear()
-            #pen.write(f"{left}  :  {right}",align="center", font=("Arial", 24, "normal"))
-            if left > 4 or right > 4:
-                quit()
-            ball.goto(0,0)
-            ball.dx *= -1
-            if ball.dx > 0:
-                ball.dx = 3
-            else:
-                ball.dx = -3
-            if ball.dy > 0:
-                ball.dy = 3
-            else:
-                ball.dy = -3
-
-            delay = True
-
-            # check the paddles
-        if (ball.xcor() > 330 and ball.xcor() < 350) and (ball.ycor() < pad_R.ycor() + 50 and ball.ycor() > pad_R.ycor() -50):
-            ball.setx(330)
-            ball.dx *= -1
-        if (ball.xcor() < -330 and ball.xcor() > -350 ) and (ball.ycor() < pad_L.ycor() + 50 and ball.ycor() > pad_L.ycor() -50):
-            ball.setx(-330)
-            ball.dx *= -1
-
-        #time.sleep(0.02)
+quit()
 
 
 
